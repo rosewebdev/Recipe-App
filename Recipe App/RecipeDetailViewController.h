d2cbd8fc237740b96a30d71c3968ee8c146aba01
rosewebdev <jason@rosewebdev.com>
2015-04-06 15:19:04 -0600

    Step 5.4 and 5.5 Added new RecipeDetailViewController class and instantiated it

---
+++ Recipe App/RecipeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeDetailViewController.h
+//  Recipe App
+//
+//  Created by Jason Kupiec on 4/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeDetailViewController : UIViewController
+
+@end
--- Recipe App/RecipeDetailViewController.m
+++ Recipe App/RecipeDetailViewController.m
@@ -1,37 +1,30 @@
 //
-//  RecipeViewController.m
+//  RecipeDetailViewController.m
 //  Recipe App
 //
 //  Created by Jason Kupiec on 4/6/15.
 //  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
-#import "RecipeViewController.h"
 #import "RecipeDetailViewController.h"
 
-@interface RecipeViewController () <UITableViewDelegate>
-
-@property (strong, nonatomic)UITableView *tableView;
+@interface RecipeDetailViewController ()
 
 @end
 
-@implementation RecipeViewController
+@implementation RecipeDetailViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
-    [self.view addSubview:self.tableView];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:scrollView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 /*
