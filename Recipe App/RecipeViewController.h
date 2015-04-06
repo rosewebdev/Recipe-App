87c5b12aaa44dff465c6c0f891cbb8673caf13fa
rosewebdev <jason@rosewebdev.com>
2015-04-06 14:02:49 -0600

    Step 1.2 created UIViewcontroller called RecipeViewController

---
+++ Recipe App/RecipeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeViewController.h
+//  Recipe App
+//
+//  Created by Jason Kupiec on 4/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeViewController : UIViewController
+
+@end
---
+++ Recipe App/RecipeViewController.m
@@ -0,0 +1,37 @@
+//
+//  RecipeViewController.m
+//  Recipe App
+//
+//  Created by Jason Kupiec on 4/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "RecipeViewController.h"
+
+@interface RecipeViewController ()
+
+@end
+
+@implementation RecipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
